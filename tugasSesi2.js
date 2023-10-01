d115ac91813dad7cf46854a88404a09196f8df51
UjangSholihin03 <114728784+UjangSholihin03@users.noreply.github.com>
2023-10-01 21:24:19 +0700

    Add files via upload

---
+++ tugasSesi2.js
@@ -0,0 +1,15 @@
+
+var person = {
+    firstName    : "ujang", 
+    lastName     : "solihin",
+    age          : "20 tahun", 
+    isMarried    : "belum menikah",
+  };
+  
+  
+  console.log("Data Pribadi:");
+  console.log("First Name   :", person.firstName);
+  console.log("Last Name    :", person.lastName);
+  console.log("Age          :", person.age);
+  console.log("Is Married   :", person.isMarried);
+  
